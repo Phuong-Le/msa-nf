31a2eb035a43b38e7597ea902cbc416b7d24173f
Sergey Senkin <s.senkin@gmail.com>
2020-05-20 23:16:26 +0200

    288 context in conversion script

--- scripts/convert_SP_to_MSA.py
+++ scripts/convert_SP_to_MSA.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import copy
 import pandas as pd
 from argparse import ArgumentParser
 
@@ -42,6 +43,9 @@ if __name__ == '__main__':
         elif context==192 or context==384:
             index_col = [0,1,2]
             signatures = pd.read_csv('%s/%s_%s_192_signatures.csv' % (signature_tables_path, signatures_prefix, mutation_type), sep=None, index_col=index_col)
+        elif context==288:
+            index_col = 0
+            signatures = pd.read_csv('%s/%s_%s_288_signatures.csv' % (signature_tables_path, signatures_prefix, mutation_type), sep=None, index_col=index_col)
         else:
             raise ValueError("Context %i is not supported." % context)
     else:
@@ -52,6 +56,16 @@ if __name__ == '__main__':
     else:
         suffix = 'all'
 
+    # reindex 288 signatures
+    if context==288:
+        for element in signatures.index:
+            sub = element.split('[', 1)[1].split(']')[0]
+            replaced_element = element.replace('['+sub+']', sub[0])
+            replaced_element = replaced_element.replace(':',':' + sub + ':')
+            signatures.rename(index={element:replaced_element}, inplace=True)
+        signatures.index = pd.MultiIndex.from_tuples(signatures.index.str.split(':').tolist())
+        signatures.to_csv('%s/%s_%s_288_new_signatures.csv' % (signature_tables_path, signatures_prefix, mutation_type), sep = ',')
+
     input_files = glob.glob(input_path + '/*.' + suffix)
 
     for file in input_files:
@@ -60,7 +74,6 @@ if __name__ == '__main__':
             if not '96' in file:
                 continue
             input_table = input_table.sort_index(level=0)
-            new_index = input_table.index
             for element in input_table.index:
                 sub = element.split('[', 1)[1].split(']')[0]
                 replaced_element = element.replace('['+sub+']', sub[0])
@@ -73,7 +86,27 @@ if __name__ == '__main__':
                 continue
             input_table = input_table[~input_table.index.str.contains("B:")]
             input_table = input_table[~input_table.index.str.contains("N:")]
-            new_index = input_table.index
+            for element in input_table.index:
+                sub = element.split('[', 1)[1].split(']')[0]
+                replaced_element = element.replace('['+sub+']', sub[0])
+                replaced_element = replaced_element.replace(':',':' + sub + ':')
+                input_table.rename(index={element:replaced_element}, inplace=True)
+            input_table.index = pd.MultiIndex.from_tuples(input_table.index.str.split(':').tolist())
+            input_table = input_table.reindex(signatures.index)
+        if context==288:
+            if not '384' in file:
+                continue
+            # move bidirectional mutations to non-trancribed ones
+            bidirectional_mutations = copy.deepcopy(input_table)
+            bidirectional_mutations[~bidirectional_mutations.index.str.contains("B:")] = 0
+            non_transcribed_mutations = copy.deepcopy(bidirectional_mutations)
+            non_transcribed_mutations.index = non_transcribed_mutations.index.str.replace("N:","X:")
+            non_transcribed_mutations.index = non_transcribed_mutations.index.str.replace("B:","N:")
+            non_transcribed_mutations.index = non_transcribed_mutations.index.str.replace("X:","B:")
+            input_table = input_table + non_transcribed_mutations
+            # bidirectional_mutations_halved = round(bidirectional_mutations/2,0)
+            # bidirectional_mutations_halved = bidirectional_mutations_halved.astype(int)
+            input_table = input_table[~input_table.index.str.contains("B:")]
             for element in input_table.index:
                 sub = element.split('[', 1)[1].split(']')[0]
                 replaced_element = element.replace('['+sub+']', sub[0])
