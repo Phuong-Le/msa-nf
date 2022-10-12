4c2f40327073e099673bbb6ef430763ebafee17b
Sergey Senkin <s.senkin@gmail.com>
2022-10-12 17:22:04 +0200

    Adding confidence intervals on sensitivity/specificity in regular outputs for simulated data

--- bin/plot_metrics.py
+++ bin/plot_metrics.py
@@ -34,7 +34,7 @@ def make_lineplot(x_values, y_values, xlabel, ylabel, title='', savepath="./line
     plt.savefig(savepath, transparent=True)
     plt.close()
 
-def make_boxplot(input_table, title, xlabel, ylabel, show_mean=False, ylim_zero_to_one=True, add_jitter = False, savepath="./boxplot.pdf"):
+def make_boxplot(input_table, title, xlabel, ylabel, show_mean=False, ylim_zero_to_one=True, add_jitter = False, savepath="./boxplot.pdf", sensitivity_CIs = None, specificity_CIs = None):
     global confidence_level
     make_folder_if_not_exists(savepath.rsplit('/', 1)[0])
 
@@ -58,7 +58,35 @@ def make_boxplot(input_table, title, xlabel, ylabel, show_mean=False, ylim_zero_
             y = table[d]
             x = np.random.normal(i+1, 0.04, len(y))
             plt.plot(x, y, mec='k', ms=3, marker="o", linestyle="None")
+    
+    if 'Sensitivity' in columns and sensitivity_CIs is not None:
+        errorbars = np.abs(sensitivity_CIs-table['Sensitivity'][0])
+        errorbars = [[sensitivity_CIs['lower_CL'][0]],[sensitivity_CIs['upper_CL'][0]]]
+        plt.errorbar(table.columns.get_loc("Sensitivity")+1, table['Sensitivity'][0],  yerr=errorbars, color='black', capsize=5)
+    
+    if 'Specificity' in columns and specificity_CIs is not None:
+        errorbars = np.abs(specificity_CIs-table['Specificity'][0])
+        errorbars = [[specificity_CIs['lower_CL'][0]],[specificity_CIs['upper_CL'][0]]]
+        plt.errorbar(table.columns.get_loc("Specificity")+1, table['Specificity'][0], yerr=errorbars, color='black', capsize=5)
 
+    if 'Sensitivity' not in columns and sensitivity_CIs is not None:
+        sensitivity_CIs_dataframe = pd.DataFrame(columns=['lower_CL','upper_CL'])
+        for signature in sensitivity_CIs.keys():
+            sensitivity_CIs_dataframe.loc[signature] = sensitivity_CIs[signature].loc['0']
+        errorbars_dataframe = sensitivity_CIs_dataframe.T-table.loc['0']
+        errorbars_dataframe = errorbars_dataframe.reindex(columns=table.columns)
+        errorbars = np.abs(errorbars_dataframe)
+        plt.errorbar([table.columns.get_loc(c)+1 for c in table.columns], table.loc['0'], yerr=np.array(errorbars), color='black', linestyle='', capsize=5)
+
+    if 'Specificity' not in columns and specificity_CIs is not None:
+        specificity_CIs_dataframe = pd.DataFrame(columns=['lower_CL','upper_CL'])
+        for signature in specificity_CIs.keys():
+            specificity_CIs_dataframe.loc[signature] = specificity_CIs[signature].loc['0']
+        errorbars_dataframe = specificity_CIs_dataframe.T-table.loc['0']
+        errorbars_dataframe = errorbars_dataframe.reindex(columns=table.columns)
+        errorbars = np.abs(errorbars_dataframe)
+        plt.errorbar([table.columns.get_loc(c)+1 for c in table.columns], table.loc['0'], yerr=np.array(errorbars), color='black', linestyle='', capsize=5)
+    
     plt.xticks(rotation=90)
     # Add a horizontal grid to the plot
     ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
@@ -160,6 +188,9 @@ if __name__ == '__main__':
     # signatures to consider
     signatures_to_consider = list(central_attribution_table_abs.columns)
 
+    signatures_prevalences = pd.read_csv(input_attributions_folder + '/signatures_prevalences_' + dataset_name + '_' + mutation_type + '.csv', index_col=0)
+    make_boxplot(signatures_prevalences, 'Signature prevalence (%s)' % mutation_type, 'Signatures', 'Reconstructed (%)', savepath=output_folder + '/signature_prevalences.pdf')
+
     # read truth-related dataframes and dictionaries of calculated variables
     if 'SIM' in dataset_name:
         truth_and_measured_difference = pd.read_csv(input_attributions_folder + '/truth_studies/truth_and_measured_difference_' + mutation_type + '.csv', index_col=0)
@@ -167,31 +198,35 @@ if __name__ == '__main__':
         sensitivity_thresholds = pd.read_csv(input_attributions_folder + '/truth_studies/sensitivity_thresholds_' + mutation_type + '.csv', index_col=0)
         stat_scores_tables = pd.read_csv(input_attributions_folder + '/truth_studies/stat_scores_tables_' + mutation_type + '.csv', index_col=0)
         stat_scores_from_CI_tables = pd.read_csv(input_attributions_folder + '/truth_studies/stat_scores_from_CI_tables_' + mutation_type + '.csv', index_col=0)
+        sensitivity_CI_tables = pd.read_csv(input_attributions_folder + '/truth_studies/sensitivity_CI_tables_' + mutation_type + '.csv', index_col=0)
+        sensitivity_CI_from_CI_tables = pd.read_csv(input_attributions_folder + '/truth_studies/sensitivity_CI_from_CI_tables_' + mutation_type + '.csv', index_col=0)
+        specificity_CI_tables = pd.read_csv(input_attributions_folder + '/truth_studies/specificity_CI_tables_' + mutation_type + '.csv', index_col=0)
+        specificity_CI_from_CI_tables = pd.read_csv(input_attributions_folder + '/truth_studies/specificity_CI_from_CI_tables_' + mutation_type + '.csv', index_col=0)
         signatures_CPs_dict = read_data_from_JSON(input_attributions_folder + '/truth_studies/signatures_CPs_dict_' + mutation_type + '.json')
         signatures_scores = read_data_from_JSON(input_attributions_folder + '/truth_studies/signatures_scores_' + mutation_type + '.json')
         signatures_scores_from_CI = read_data_from_JSON(input_attributions_folder + '/truth_studies/signatures_scores_from_CI_' + mutation_type + '.json')
         stat_scores_per_sig = read_data_from_JSON(input_attributions_folder + '/truth_studies/stat_scores_per_sig_' + mutation_type + '.json')
         stat_scores_from_CI_per_sig = read_data_from_JSON(input_attributions_folder + '/truth_studies/stat_scores_from_CI_per_sig_' + mutation_type + '.json')
+        sensitivity_CI_from_CI_per_sig = read_data_from_JSON(input_attributions_folder + '/truth_studies/sensitivity_CI_from_CI_per_sig_' + mutation_type + '.json')
+        sensitivity_CI_per_sig = read_data_from_JSON(input_attributions_folder + '/truth_studies/sensitivity_CI_per_sig_' + mutation_type + '.json')
+        specificity_CI_from_CI_per_sig = read_data_from_JSON(input_attributions_folder + '/truth_studies/specificity_CI_from_CI_per_sig_' + mutation_type + '.json')
+        specificity_CI_per_sig = read_data_from_JSON(input_attributions_folder + '/truth_studies/specificity_CI_per_sig_' + mutation_type + '.json')
 
-    # plot sensitivity threshold vs CP plot for each sig
-    if 'SIM' in dataset_name:
+        # plot sensitivity threshold vs CP plot for each sig
         for signature in signatures_to_consider:
             signature_attribution_thresholds = sorted(signatures_CPs_dict.keys())
             CPs = [signatures_CPs_dict[threshold][signature][0] for threshold in signature_attribution_thresholds]
             make_lineplot(signature_attribution_thresholds, CPs, 'Sensitivity threshold', 'Confidence probability', 'Signature %s' % signature, output_folder + '/truth_studies/CP_curves/signature_%s_curve.pdf' % signature)
-
-    signatures_prevalences = pd.read_csv(input_attributions_folder + '/signatures_prevalences_' + dataset_name + '_' + mutation_type + '.csv', index_col=0)
-    make_boxplot(signatures_prevalences, 'Signature prevalence (%s)' % mutation_type, 'Signatures', 'Reconstructed (%)', savepath=output_folder + '/signature_prevalences.pdf')
-
-    if 'SIM' in dataset_name:
+        
+        # plot all other metrics
         make_boxplot(signatures_CPs, 'Confidence probability (%s)' % mutation_type, 'Signatures', 'CP (%)', savepath=output_folder + '/truth_studies/signature_confidence_probabilities.pdf')
         make_boxplot(sensitivity_thresholds, 'Sensitivity thresholds (%s)' % mutation_type, 'Signatures', 'Threshold (%)', ylim_zero_to_one = False, savepath=output_folder + '/truth_studies/sensitivity_thresholds.pdf')
-        make_boxplot(stat_scores_tables, 'Metrics (%s)' % mutation_type, '', 'Values', savepath=output_folder + '/truth_studies/sensitivity_specificity_metrics.pdf')
-        make_boxplot(stat_scores_from_CI_tables, 'CI-based metrics (%s)' % mutation_type, '', 'Values', savepath=output_folder + '/truth_studies/sensitivity_specificity_from_CI_metrics.pdf')
+        make_boxplot(stat_scores_tables, 'Metrics (%s)' % mutation_type, '', 'Values', savepath=output_folder + '/truth_studies/sensitivity_specificity_metrics.pdf', sensitivity_CIs = sensitivity_CI_tables, specificity_CIs = specificity_CI_tables)
+        make_boxplot(stat_scores_from_CI_tables, 'CI-based metrics (%s)' % mutation_type, '', 'Values', savepath=output_folder + '/truth_studies/sensitivity_specificity_from_CI_metrics.pdf', sensitivity_CIs = sensitivity_CI_from_CI_tables, specificity_CIs = specificity_CI_from_CI_tables)
         for signature in signatures_to_consider:
-            make_boxplot(stat_scores_per_sig[signature], 'Signature %s metrics (%s)' % (signature, mutation_type), '', 'Values', savepath=output_folder + '/truth_studies/per_signature_metrics/%s_metrics.pdf' % signature)
-            make_boxplot(stat_scores_from_CI_per_sig[signature], 'Signature %s CI metrics (%s)' % (signature, mutation_type), '', 'Values', savepath=output_folder + '/truth_studies/per_signature_from_CI_metrics/%s_metrics.pdf' % signature)
+            make_boxplot(stat_scores_per_sig[signature], 'Signature %s metrics (%s)' % (signature, mutation_type), '', 'Values', savepath=output_folder + '/truth_studies/per_signature_metrics/%s_metrics.pdf' % signature, sensitivity_CIs = sensitivity_CI_per_sig[signature], specificity_CIs = specificity_CI_per_sig[signature])
+            make_boxplot(stat_scores_from_CI_per_sig[signature], 'Signature %s CI metrics (%s)' % (signature, mutation_type), '', 'Values', savepath=output_folder + '/truth_studies/per_signature_from_CI_metrics/%s_metrics.pdf' % signature, sensitivity_CIs = sensitivity_CI_from_CI_per_sig[signature], specificity_CIs = specificity_CI_from_CI_per_sig[signature])
         for score in scores:
-            make_boxplot(signatures_scores[score], '%s (%s)' % (score, mutation_type), 'Signatures', 'Values', savepath=output_folder + '/truth_studies/signature_%s.pdf' % score)
-            make_boxplot(signatures_scores_from_CI[score], 'CI-based %s (%s)' % (score, mutation_type), 'Signatures', 'Values', savepath=output_folder + '/truth_studies/signature_%s_from_CI.pdf' % score)
+            make_boxplot(signatures_scores[score], '%s (%s)' % (score, mutation_type), 'Signatures', 'Values', savepath=output_folder + '/truth_studies/signature_%s.pdf' % score, sensitivity_CIs = sensitivity_CI_per_sig if 'Sensitivity' in score else None, specificity_CIs = specificity_CI_per_sig if 'Specificity' in score else None)
+            make_boxplot(signatures_scores_from_CI[score], 'CI-based %s (%s)' % (score, mutation_type), 'Signatures', 'Values', savepath=output_folder + '/truth_studies/signature_%s_from_CI.pdf' % score, sensitivity_CIs = sensitivity_CI_from_CI_per_sig if 'Sensitivity' in score else None, specificity_CIs = specificity_CI_from_CI_per_sig if 'Specificity' in score else None)
         make_boxplot(truth_and_measured_difference, 'Truth and measured difference (%s)' % mutation_type, '', 'Difference', ylim_zero_to_one=False, add_jitter = True, savepath=output_folder + '/truth_studies/truth_and_measured_difference.pdf')
